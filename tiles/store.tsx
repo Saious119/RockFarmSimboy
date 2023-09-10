f195c77948ecf6294dcdd14da08766924af85985
Andy Mahoney <32443674+Saious119@users.noreply.github.com>
2023-09-10 18:15:47 -0400

    reduce planted to 6, mart and basic dialog

---
+++ tiles/store.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="store" tilewidth="8" tileheight="8" tilecount="325" columns="13">
+ <image source="C:/Users/sht13/Downloads/seven_eleven_gbstudio_tileset/seven_eleven_tileset.png" width="104" height="200"/>
+</tileset>
